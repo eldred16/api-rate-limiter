ac3e362ca119328422ec6447d8c5beaf952f1a9b
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-19 15:20:11 +0300

    Removed the erors that were existing in the file i had pushed

--- rate-limiter-service/src/main/java/com/example/ratelimiterservice/service/RedisRateLimiterService.java
+++ rate-limiter-service/src/main/java/com/example/ratelimiterservice/service/RedisRateLimiterService.java
@@ -24,12 +24,10 @@ public class RedisRateLimiterService {
     /**
      * Constructs a RedisRateLimiterService with the given RedisTemplate,
      * RedisScript, and rate limiter properties.
-     * 
-     * @param redisTemplate         the Redis template used for executing Redis
-     *                              operation
+     * @param redisTemplate         the Redis template used for executing Redis                              operation
      * @param redisScript           the Redis script used for rate limiting logic
      * @param rateLimiterProperties configuration properties for rate limiting
-     *                              (bucket size, refill rate)
+                                     (bucket size, refill rate)
      */
 
     public RedisRateLimiterService(RedisTemplate<String, String> redisTemplate, RedisScript<Long> redisScript,
@@ -38,11 +36,9 @@ public class RedisRateLimiterService {
         this.redisScript = redisScript;
         this.rateLimiterProperties = rateLimiterProperties;
     }
-
     /**
      * checks if the given key (e.g client IP) is allowed to make a request using
      * the default of 1 token
-     * 
      * @param key the key representing a client (forexample IP address)
      * @return true if allowed, false if the rate limit has been exceeded
      */
@@ -57,7 +53,6 @@ public class RedisRateLimiterService {
      * <p>
      * This method uses a Redis script to enforce rate limits and is decorated with
      * a circuitbreaker and retry
-     * 
      * @param key             the key representing a client (forexample IP address)
      * @param requestedTokens the number of tokens requested for this operation
      * @return true if allowed, false if the rate limit has been exceeded
@@ -87,7 +82,6 @@ public class RedisRateLimiterService {
      * error occurs
      * <p>
      * Always return true toi allow the request when the circuit is open
-     * 
      * @param key the key representing a client (forexample IP address)
      * @param t   the Throwable that triggered the fallback
      * @return true to allow the request despite the failure
